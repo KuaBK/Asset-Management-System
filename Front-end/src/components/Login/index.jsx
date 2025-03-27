8432d283f9723ed5e7c9dccb0ae4f1590bdca19b
rabbit-204 <phongthanhlk18@gmail.com>
2025-03-27 22:02:06 +0700

    handle error login

--- Front-end/src/components/Login/index.jsx
+++ Front-end/src/components/Login/index.jsx
@@ -4,6 +4,7 @@ import hcmutbg from '../../assets/bg-hcmut.svg'
 import { useNavigate } from 'react-router-dom';
 import eye from '../../assets/eye.svg'
 import eyeBlind from '../../assets/eye-blind.svg'
+import Swal from 'sweetalert2'
 
 const Login = () => {
     const navigate = useNavigate();
@@ -19,6 +20,22 @@ const Login = () => {
             username: userName,
             password: password
         }
+        if(form.username==""){
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Please enter username!"
+              });
+              return
+        }
+        if(form.password==""){
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Please enter password!"
+              });
+              return
+        }
         try {
             const response = await fetch("http://localhost:8080/api/auth/login", {
                 method: "POST",
@@ -29,7 +46,11 @@ const Login = () => {
             });
 
             if (!response.ok) {
-
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Tài khoản mật khẩu chưa đúng!"
+                  });
                 throw new Error("Login failed");
             }
 
@@ -63,7 +84,7 @@ const Login = () => {
                             placeholder=""
                             value={userName}
                             onChange={(e) => setUserName(e.target.value)}
-                            className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-[#5BA1F2] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                            className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-[#5BA1F2] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         />
                         <label
                             className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-[#5BA1F2] peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
@@ -77,14 +98,14 @@ const Login = () => {
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             type={showPW?'text':'password'}
-                            className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-[11px] font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-[#5BA1F2] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                            className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-[#5BA1F2] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         />
                         <label
                             className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-[#5BA1F2] peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
                         >
                             Password
                         </label>
-                        <img src={!showPW?eye:eyeBlind} onClick={handleShowPassWord} className='absolute top-1 right-2' alt="" />
+                        <img src={!showPW?eye:eyeBlind} onClick={handleShowPassWord} className='absolute top-1.5 right-2' alt="" />
 
                     </div>
                     <a href="#" className="text-blue-400 text-[16px] transition hover:underline text-start"
@@ -103,7 +124,7 @@ const Login = () => {
                     >
                         Sign In
                     </button>
-                    <div className="mt-4 text-center text-[16px] text-gray-500">
+                    <div className="mt-4 text-center text-[16px] text-black">
                     <p>
                         By signing in, you agree to our
                         <a href="#" className="text-blue-400 transition px-[3px] hover:underline"
